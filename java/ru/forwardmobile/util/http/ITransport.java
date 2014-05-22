47082afec3265c237fbcbc91458faf8db3ff97d0
igor.zont <igor.zont@563276c8-fc6f-df90-85c4-ce57b7db7bff>
2014-05-22 11:23:34 +0000

    Initial commit
    git-svn-id: https://github.com/unuservize/ForwardAndroid.git/trunk@2 563276c8-fc6f-df90-85c4-ce57b7db7bff

--- java/ru/forwardmobile/util/http/ITransport.java
+++
@@ -1,7 +0,0 @@
-package ru.forwardmobile.util.http;
-
-public interface ITransport {
-
-        public IResponse send(IRequest request) throws Exception;
-        
-}
--- java/ru/forwardmobile/util/http/TransportFactory.java
+++
@@ -1,9 +0,0 @@
-package ru.forwardmobile.util.http;
-
-public class TransportFactory {
-
-    public static ITransport getTransport() {
-            TransportImpl transport = new TransportImpl();
-            return transport;
-    }
-}
