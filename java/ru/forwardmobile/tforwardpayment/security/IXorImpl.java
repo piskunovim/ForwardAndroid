ebda65ae8492159ca880146329fd8c4128118291
unuservize <unuservize@563276c8-fc6f-df90-85c4-ce57b7db7bff>
2014-06-24 08:45:00 +0000

    - adds
    git-svn-id: https://github.com/unuservize/ForwardAndroid.git/trunk@63 563276c8-fc6f-df90-85c4-ce57b7db7bff

---
+++ java/ru/forwardmobile/tforwardpayment/security/IXorImpl.java
@@ -0,0 +1,29 @@
+package ru.forwardmobile.tforwardpayment.security;
+
+/**
+ * Created by PiskunovI on 23.06.14.
+ */
+public class IXorImpl {
+
+    public byte[] encrypt(String text, String keyWord)
+    {
+        byte[] arr = text.getBytes();
+        byte[] keyarr = keyWord.getBytes();
+        byte[] result = new byte[arr.length];
+        for(int i = 0; i< arr.length; i++)
+        {
+            result[i] = (byte) (arr[i] ^ keyarr[i % keyarr.length]);
+        }
+        return result;
+    }
+    public String decrypt(byte[] text, String keyWord)
+    {
+        byte[] result  = new byte[text.length];
+        byte[] keyarr = keyWord.getBytes();
+        for(int i = 0; i < text.length;i++)
+        {
+            result[i] = (byte) (text[i] ^ keyarr[i% keyarr.length]);
+        }
+        return new String(result);
+    }
+}
