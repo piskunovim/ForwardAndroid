e47ce67651005bff5e818db19efeeacf7a1f2f7c
Василий Ванин <alpineest@mail.ru>
2014-08-15 17:06:29 +0400

    Xml plugins

---
+++ java/ru/raketa/util/xml/AbstractXmlDeserializable.java
@@ -0,0 +1,44 @@
+package ru.raketa.util.xml;
+
+import org.xmlpull.v1.XmlPullParser;
+
+import java.util.Date;
+
+import ru.raketa.util.Dates;
+
+/**
+ * Syntax Sugar thing
+ * Created by Василий Ванин on 25.07.14.
+ */
+public abstract class AbstractXmlDeserializable implements IXmlDeserializable {
+
+    /**
+     * Safe getter for integer values
+     * @param parser
+     * @param name
+     * @return
+     */
+    protected int readInt(XmlPullParser parser, String name) {
+        try {
+            return Integer.valueOf(parser.getAttributeValue(null, name));
+        } catch(Exception ex) {
+            return 0;
+        }
+    }
+
+    protected Date readDate(XmlPullParser parser, String name) {
+        try {
+            return Dates.fromSqlDatetime(parser.getAttributeValue(null,name));
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    protected boolean readBoolean(XmlPullParser parser, String name) {
+        try {
+            return Boolean.valueOf(parser.getAttributeValue(null, name));
+        }catch (Exception ex) {
+            return false;
+        }
+    }
+}
